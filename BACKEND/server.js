c50ce045bbc5f6c95c5607172f147407d2006bac
Swastika Ghosh <swastika290305@gmail.com>
2025-08-25 22:25:50 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app= express()//initialiazation of express
-const PORT=process.env.PORT;
+// const PORT=process.env.PORT;
 app.use(cors());
 
 app.use(express.json());//build-in-middelware
@@ -20,10 +20,14 @@ mongoose.connect(process.env.MONGODB_URI)
 app.use('/',userRoutes)
 
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-app.listen(5000,()=>{
-   console.log("server running on port 5000")
-})//port,callback function
+// app.listen(5000,()=>{
+//    console.log("server running on port 5000")
+// })//port,callback function
 
 
 
